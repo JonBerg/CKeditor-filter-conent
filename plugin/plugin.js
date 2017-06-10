aabba07fa75104e1cab5ee9036f92ec7e0dd357f
Gints Erglis <gints.erglis@wunderkraut.com>
2017-06-10 03:48:51 +0300

    plugin code

--- plugin/plugin.js
+++ plugin/plugin.js
@@ -1,6 +1,24 @@
-(function($) {
-console.log("start");
-CKEDITOR.replace( 'ckeditorCustomJS', {
-  extraAllowedContent: 'u;span{color}'
-} );
-})(jQuery);
+(function ($, Drupal, drupalSettings, CKEDITOR) {
+
+  'use strict';
+  CKEDITOR.plugins.add( 'ckeditorCustomJS',{
+    icons : '', // optional
+    init : function( editor ) {
+
+      var filter = new CKEDITOR.filter( 'p strong em br h2' );
+      editor.setActiveFilter( filter );
+
+      editor.on('beforePaste', function (event) {
+        //this.setActiveFilter( filter );
+        this.setMode( 'source' );
+        console.log('before paste');
+      });
+      editor.on('afterPaste', function (event) {
+        this.setMode( 'wysiwyg' );
+        console.log('after paste');
+      });
+
+    }
+  })
+
+})(jQuery, Drupal, drupalSettings, CKEDITOR);
